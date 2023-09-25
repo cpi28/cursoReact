007d330809502b546044471bd34490597a265268
cpi28 <carla.perez@estudiantes.utec.edu.uy>
2023-09-25 18:53:00 -0300

    EmailJS

--- src/routes/Contacto.jsx
+++ src/routes/Contacto.jsx
@@ -1,5 +1,10 @@
-export default function Contacto(){
-    return(
-        <h1>Formulario de Contacto</h1>
-    )
+import Form from "../components/Form";
+
+export default function Contacto() {
+  return (
+    <>
+      <h1>Formulario de Contacto</h1>
+      <Form />
+    </>
+  );
 }
