81dfe1f7cef48275ada68c59bfd3402e0711ff77
cpi28 <carla.perez@estudiantes.utec.edu.uy>
2023-09-26 18:29:20 -0300

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cursoReact/',
   plugins: [react()],
   build: {
     outDir: 'dist',
